a0ccb37dc8ed9cb440d49b8e632f0413e22a36b2
Amreen Shaikh <amreen.rajmohmad@khoros.com>
2022-04-11 18:15:59 +0530

    basic schema of string

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import schema from "./schema";
+import { graphqlHTTP } from 'express-graphql';
+
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = { hello: () => "Hi, I am Amreen!"};
+
+app.use('/graphql', graphqlHTTP ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8000, () => {
     console.log('listening on http://localhost:8000/graphql');
