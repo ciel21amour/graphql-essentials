8adc23f9d5efdf07c31a83653a42237ae530a548
Amreen Shaikh <amreen.rajmohmad@khoros.com>
2022-05-09 15:07:06 +0530

    resolvers

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 import express from 'express';
 import schema from './schema';
 import { graphqlHTTP } from 'express-graphql';
-
-
-const friendDatabase = {};
-
-class Friend {
-    constructor(id, {firstName, lastName, gender, email}) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.email = email;
-    }
-}
-
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -22,22 +9,7 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
 
-// resolver
-const root = { friend : () => {
-    return { 
-        "id" : 567,
-        "firstName" : "Amreen",
-        "lastName" : "Shaikh",
-        "gender" : "Female",
-        "email" : "amreen.shaikh.21.05@gmail.com"}
-    }, 
-    createFriend : ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
-
-    }
-};
+const root = resolvers;
 
 app.use('/graphql', graphqlHTTP ({
     schema: schema,
